c198632cf29d368fb45dadfd4a9389c8fdc6cbb9
Emmanuel Sarella <emmanuels@gmail.com>
2016-03-18 10:46:14 -0400

    End of section 2

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -7,13 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
 
+    var audioPlayer:AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        loadAudioFile("movie_quote", fileType: "mp3")
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +23,38 @@ class PlaySoundsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func playSlowAudio(sender: UIButton) {
+        playAudio(0.5)
+    }
+    
+    @IBAction func playFastAudio(sender: UIButton) {
+        playAudio(2.0)
+    }
+    
+    @IBAction func stopAudio(sender: UIButton) {
+        audioPlayer.stop()
+    }
+    
+    func playAudio(rate: Float)
+    {
+        audioPlayer.stop()
+        audioPlayer.rate = rate
+        audioPlayer.currentTime = 0
+        audioPlayer.play()
+    }
+    
+    func loadAudioFile(fileName: String, fileType: String){
+        do{
+            let filePath = NSBundle.mainBundle().pathForResource(fileName, ofType: fileType)
+            let filePathURL = NSURL.fileURLWithPath(filePath!)
+            
+            audioPlayer = try AVAudioPlayer(contentsOfURL:filePathURL)
+            audioPlayer.enableRate = true
+        }
+        catch{
+            print("Error fetching audio file")
+        }
+    }
 
     /*
     // MARK: - Navigation
